3037d0a8c6e41083fb3d4cc4e57dddfa7d979c0a
Pavel Grigorenko <pgrigorenko@gmail.com>
2021-09-16 11:50:06 +0200

    added failing case with spy in a method having a space in the name

--- src/test/kotlin/com/example/Test.kt
+++ src/test/kotlin/com/example/Test.kt
@@ -3,8 +3,10 @@ package com.example
 import org.mockito.kotlin.spy
 import reactor.core.publisher.Flux
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class Test {
 
@@ -13,6 +15,20 @@ class Test {
         val deferredSupplier = spy(java.util.function.Supplier { Flux.empty<String>() })
         Flux.defer(deferredSupplier).blockLast()
         verify(deferredSupplier, times(1)).get()
-        println("end of test")
+    }
+
+    @Test
+    fun `test spy`() {
+        val deferredSupplier = spy(java.util.function.Supplier { Flux.empty<String>() })
+        Flux.defer(deferredSupplier).blockLast()
+        verify(deferredSupplier, times(1)).get()
+    }
+
+    @Test
+    fun `test mock`() {
+        val mock: java.util.function.Supplier<String> = mock()
+        whenever(mock.get()).thenReturn("")
+        mock.get()
+        verify(mock, times(1)).get()
     }
 }
